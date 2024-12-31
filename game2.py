844aba81051c995250c69e5b96fc877a280c2189
KHUSHALMAHAJAN <khushalmahajan303@gmail.com>
2024-12-31 18:53:36 +0530

    update game 2

--- game2.py
+++ game2.py
@@ -1,12 +1,32 @@
 import random
-import time
 import os
+import platform
 
-print("play with computer \nstone paper scissor")
+def clear_console():
+    if platform.system() == "Windows":
+        os.system('cls')  
+    else:
+        os.system('clear')  
+
+def option_func(option):
+    match option:
+        case 1:
+            return "stone"
+        case 2:
+            return "paper"
+        case 3:
+            return "scissor"
+        case _:
+            print("plz enter valid value")
+            return None
+
+print("Play with Computer\nStone Paper Scissor")
 name = input("Enter your name: ")
-print("Hello, " + name + " Let's play game")
+print("Hello, " + name + " Let's play the game")
 
-with open("gamehistroy.txt","a") as file:
+if os.path.exists("gamehistory.txt"):
+    os.remove("gamehistory.txt")
+with open("gamehistory.txt", "a") as file:
     file.write("\nName :- " + name)
 
 computeroption = "not use"
@@ -14,122 +34,65 @@ userscore = 0
 computerscore = 0
 useroption = 0
 repeat = "yes"
-playgame = "yes"
 i = 0
-winner = True
+winner = 0
 
-import os
-import platform
+while repeat == "yes":
+    i += 1
+    print("\nEnter a choice: Stone, Paper, or Scissor")
+    try:
+        option = int(input("1: Stone\n2: Paper\n3: Scissor\nEnter a number: "))
+        useroption = option_func(option)
+        if not useroption:
+            continue
+    except ValueError:
+        print("Invalid input! Please enter a valid number.")
+        continue
 
-def clear_console():
-    # Check the operating system
-    if platform.system() == "Windows":
-        os.system('cls')  # Clear console for Windows
+    print("You selected: " + useroption)
+    print("Wait for computer's turn...")
+    random_integer = random.randint(1, 3)
+    computeroption = option_func(random_integer)
+    print("Computer selected: " + computeroption)
+
+    if computeroption == useroption:
+        print("Match Draw")
+        winner = 2
+    elif (useroption == "stone" and computeroption == "scissor") or \
+         (useroption == "paper" and computeroption == "stone") or \
+         (useroption == "scissor" and computeroption == "paper"):
+        print(name + " wins this round!")
+        userscore += 1
+        winner = 1
     else:
-        os.system('clear')
+        print("Computer wins this round!")
+        computerscore += 1
+        winner = 0
 
-while(repeat == "yes"):
-            i += 1
-            time.sleep(2)
-            print("\n\nEnter a stone paper scissor")
-            try:
-                option = int(input("1: Stone\n2: Paper\n3: Scissor\nEnter a number: "))
-                match option:
-                    case 1:
-                        useroption = "stone"
-                    case 2:
-                        useroption = "paper"
-                    case 3:
-                        useroption = "scissor"
-                    case _:
-                        print("plz enter valid value")
-                        continue
-            except ValueError:
-                print("Invalid input! Please enter a valid number.")
-                continue
-            print("you are select "+ useroption)
-    
-            print("wait for computer turn....")
-            random_integer = random.randint(1,3)
-            match random_integer:
-                case 1:
-                    computeroption = "stone"
-                case 2:
-                    computeroption = "paper"
-                case 3:
-                    computeroption = "scissor"
-                case _:
-                    print("plz enter valid value")
-            print("computer is select " + computeroption)
+    strus = str(userscore)
+    print("\n" + name + " Score: " + strus)
+    strcs = str(computerscore)
+    print("Computer Score:  " + strcs)
 
-            if(computeroption == "stone"):
-                match useroption:
-                    case 'stone':
-                        print("match drow")
-                    case 'paper':
-                        print(name + " win")
-                        userscore += 1
-                        winner = True
-                    case 'scissor':
-                        print("computer win")
-                        computerscore += 1
-                        winner = False
+    with open("gamehistory.txt", "a") as file:
+        stri = str(i)
+        file.write("\nRound: " + stri + "\n")
+        file.write("You selected: " + useroption + "\n")
+        file.write("Computer selected: " + computeroption + "\n")
+        if winner == 1:
+            file.write(name + " wins\n")
+        elif winner == 0:
+            file.write("Computer wins\n")
+        else:
+            file.write("Match Draw\n")
+        file.write(name + " Score: " + strus + "\n")
+        file.write("Computer Score: " + strcs + "\n")
 
-            elif(computeroption == "paper"):
-                match useroption:
-                    case 'stone':
-                        print("computer win")
-                        computerscore += 1
-                        winner = False
-                    case 'paper':
-                        print("match drow")
-                    case 'scissor':
-                        print(name + " win")
-                        userscore += 1
-                        winner = True
+    repeat = input("Do you want to play again? (yes/no): ")
 
-            elif(computeroption == "scissor"):
-                match useroption:
-                    case 'stone':
-                        print(name + " win")
-                        userscore += 1
-                        winner = True
-                    case 'paper':
-                        print("computer win")
-                        computerscore += 1
-                        winner = False
-                    case 'scissor':
-                        print("match drow")
-
-            time.sleep(2.5)
-            strus = str(userscore)
-            print("\n\n" + name + " score is " + strus)
- 
-            strcs = str(computerscore)
-            print("\ncomputer score is " + strcs)
-
-            with open("gamehistroy.txt","a") as file:
-                file.write("\n")
-                stri = str(i)
-                file.write("Round :- " + stri +"\n")
-                file.write("your are select :- " + useroption + "\n")
-                file.write("computer select :- " + computeroption + "\n")
-                if(winner == True):
-                    file.write(name + " win\n")
-                else:
-                    file.write("computer win\n")
-
-                file.write("your score is :- " + strus + "\n") 
-                file.write("computer score is :- " + strcs +'\n')
-
-            repeat = input("you repeat is game \n yes/no: ")
-            if(repeat == "yes"):
-                clear_console()
-                print("Best of lauck")
-            else:
-                clear_console()
-                with open("gamehistroy.txt","r") as file:
-                    content = file.read()
-                    print("see your previous game  :- \n")
-                    print(content)
-                os.remove("gamehistroy.txt")
+if repeat == "no":
+    clear_console()
+    with open("gamehistory.txt", "r") as file:
+        content = file.read()
+    print("See your previous game history:\n")
+    print(content)
